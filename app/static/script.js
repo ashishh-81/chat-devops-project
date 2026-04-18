c2a92870b6ccbaf0f90f2bcb3d2086eb9747e791
Ashish Sharma <ashish200430@gmail.com>
2026-04-18 15:53:16 +0530

    fresh clean commit

---
+++ app/static/script.js
@@ -0,0 +1,13 @@
+// static/script.js
+var socket = io();
+
+function sendMsg() {
+  let msg = document.getElementById("msg").value;
+  socket.send(msg);
+}
+
+socket.on('message', function(msg) {
+  let li = document.createElement("li");
+  li.innerText = msg;
+  document.getElementById("messages").appendChild(li);
+});
---
+++ terraform/main.tf
@@ -0,0 +1,40 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_security_group" "chat_sg" {
+  name = "chat-sg"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "chat_server" {
+  ami           = "ami-0f5ee92e2d63afc18"
+  instance_type = "t3.micro"
+  key_name      = "chat-key"
+
+  vpc_security_group_ids = [aws_security_group.chat_sg.id]
+
+  tags = {
+    Name = "ChatServer"
+  }
+}
